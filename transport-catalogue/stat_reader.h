f706fa91f93478a44c8196be573158064b5a67a8
RanzhanK <ranzhan@yandex.ru>
2023-05-01 13:59:20 +0300

    Create namecpace in all project

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,12 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+#include <vector>
+#include <iostream>
+
+using global::transport_catalogue::TransportCatalogue;
+
+namespace global::stat_reader {
+    void GetDataBaseInfo(std::istream& input, TransportCatalogue& transport_catalogue, std::ostream& out = std::cout);
+}
